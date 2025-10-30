4e31c165840a1817e3eeed6ddf2a077ce589e7ad
Nao Takahashi <s24c1073hs@s.chibakoudai.jp>
2025-10-30 12:46:48 +0900

    Add test

--- test.bash
+++ test.bash
@@ -1,13 +1,16 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Nao Takahashi
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=高橋
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 高橋 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
+
