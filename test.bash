53d6a55ab779e444bfa1e6a55d1e31e13e9bc5e0
Nao Takahashi <s24c1073hs@s.chibakoudai.jp>
2025-11-30 14:18:51 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -20,6 +20,29 @@ out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+out=$(echo 0 | ./Fibonacci)
+[ "${out}" = 0 ] || ng "$LINENO"
+
+out=$(echo 1 | ./Fibonacci)
+[ "${out}" = 1 ] || ng "$LINENO"
+
+out=$(echo 5| ./Fibonacci)
+[ "${out}" = 5 ] || ng "$LINENO"
+
+out=$(echo 10| ./Fibonacci)
+[ "${out}" = 55 ] || ng "$LINENO"
+
+out=$(echo 15| ./Fibonacci)
+[ "${out}" = 610 ] || ng "$LINENO"
+
+out=$(echo あ | ./Fibonacci)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./Fibonacci)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
 
